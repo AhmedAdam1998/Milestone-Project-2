9a42dd8a260a857b6f965b19d01a080f97ccf851
Ahmed Adam <lv3qsb2v@students.codeinstitute.net>
2025-02-08 13:58:59 +0000

    Selected key HTML elements to manipulate quiz content dynamically

---
+++ NBA quiz-game/script.js
@@ -0,0 +1,9 @@
+// NBA Quiz Game Javascript
+// Select elements from the HTML
+const questionElement= document.getElementById("question");
+const answerButtons= document.getElementById("answer-buttons");
+const nextButton= document.getElementById("next-btn");
+const restartButton= document.getElementById("restart-btn");
+const scoreDisplay= document.getElementById("score");
+
+
